d85c81b943fc5c28495106fde0a490a3462ec480
chae <corkdud@naver.com>
2022-09-26 16:25:34 +0900

    220926

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const company = {
+    name: "쇼핑몰",
+    address: "부산시 부산진구",
+    tel: "123-456-7890",
+    fex: "987-654-321",
+    ceo: "Chae",
+};
+
+const profile = {
+    slogan: '많이 팔자'
+}
+
+const content = [
+    {id: 11, con: '슬로건 01', des: '아주 기이이이인 내용 01', },
+    {id: 21, con: '슬로건 02', des: '아주 기이이이인 내용 02', },
+    {id: 31, con: '슬로건 03', des: '아주 기이이이인 내용 03', },
+    {id: 41, con: '슬로건 04', des: '아주 기이이이인 내용 04', },
+    {id: 51, con: '슬로건 05', des: '아주 기이이이인 내용 05', },
+    {id: 61, con: '슬로건 06', des: '아주 기이이이인 내용 06', },
+]
+
+export { company, profile, content }
---
+++ src/pages/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+    return <div>나는 header</div>;
+};
+
+export default Header;
